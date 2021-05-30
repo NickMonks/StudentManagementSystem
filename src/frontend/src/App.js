91b33b57361bf02e14c0914cef0d5b01475cf045
NickMonks <nicolasmg94@gmail.com>
2021-05-30 13:50:29 +0100

    New footer implementation

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -7,7 +7,7 @@ import {
     Menu,
     Breadcrumb,
     Radio,
-    Table, Spin, Empty, Button, Badge, Tag, Popconfirm
+    Table, Spin, Empty, Button, Badge, Tag, Popconfirm, Divider
 } from 'antd';
 import {
     DesktopOutlined,
@@ -234,7 +234,16 @@ function App() {
                     {renderStudents()}
                 </div>
             </Content>
-            <Footer style={{textAlign: 'center'}}>By Me</Footer>
+            <Footer style={{textAlign: 'center'}}>
+                <Divider>
+                    <a
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        href="https://www.linkedin.com/in/nicolasmonkerud/">
+                        Click to see my LinkedIn Profile!
+                    </a>
+                </Divider>
+            </Footer>
         </Layout>
     </Layout>
 }
